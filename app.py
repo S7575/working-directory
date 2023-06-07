bae62cd9d942632155d602d6ba7e0016c6abcec1
S7575 <81707738+S7575@users.noreply.github.com>
2023-06-07 23:05:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,36 +22,27 @@ st.set_page_config(
 
 
 def authenticate(
-    openai_api_key: str, PINECONE_API_KEY: str, PINECONE_ENV: str
+    openai_key: str, pinecone_key: str, pinecone_env: str
 ) -> None:
     # Validate all credentials are set and correct
     # Check for env variables to enable local dev and deployments with shared credentials
-    openai_api_key = (
-        openai_api_key
-        or os.environ.get("OPENAI_API_KEY")
-        or st.secrets.get("OPENAI_API_KEY")
-    )
-    os.environ["OPENAI_API_KEY"] = openai_api_key
-    PINECONE_API_KEY = (
-        PINECONE_API_KEY
-        or os.environ.get("PINECONE_API_KEY")
-        or st.secrets.get("PINECONE_API_KEY")
-    )
-    os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-    PINECONE_ENV = (
-        PINECONE_ENV
-        or os.environ.get("PINECONE_ENV")
-        or st.secrets.get("PINECONE_ENV")
-    )
-    os.environ["PINECONE_ENV"] = PINECONE_ENV
-    if not (openai_api_key and PINECONE_API_KEY and PINECONE_ENV):
+    openai_key = st.secrets["openai"]["api_key"]
+
+    os.environ["openai_key"] = openai_key
+    pinecone_key = st.secrets["pinecone"]["api_key"]
+    
+    os.environ["pinecone_key"] = pinecone_key
+    pinecone_env = st.secrets["pinecone"]["env"]
+    
+    os.environ["pinecone_env"] = pinecone_env
+    if not (openai_key and pinecone_key and pinecone_env):
         st.session_state["auth_ok"] = False
         st.error("Credentials neither set nor stored")#, icon=PAGE_ICON)
         return
     try:
         # Try to access openai and deeplake
         with st.spinner("Authentifying..."):
-            openai.api_key = openai_api_key
+            openai.api_key = openai_key
     except Exception as e:
         logger.error(f"Authentication failed with {e}")
         st.session_state["auth_ok"] = False
@@ -59,22 +50,26 @@ def authenticate(
         return
     # store credentials in the session state
     st.session_state["auth_ok"] = True
-    st.session_state["openai_api_key"] = openai_api_key
-    st.session_state["PINECONE_API_KEY"] = PINECONE_API_KEY
-    st.session_state["PINECONE_ENV"] = PINECONE_ENV
+    st.session_state["openai_key"] = openai_key
+    st.session_state["pinecone_key"] = pinecone_key
+    st.session_state["pinecone_env"] = pinecone_env
     logger.info("Authentification successful!")
     st.sidebar.success("Authentification successful!")
     # return True
 
+    # Print the contents of the secrets file
+    # st.write(st.secrets())
+
 # # Page options and header
 # st.set_option("client.showErrorDetails", True)
+
 
 
 def main():
     st.markdown(
         """
         <div style='text-align: center;'>
-            <h1>🧠 AI Chatbot</h1>
+            <h1>🧠 AI Chatbot - mod</h1>
         </div>
         """,
         unsafe_allow_html=True,
@@ -91,7 +86,12 @@ def main():
     
 def admin():
     pinecone_index = "aichat"
-    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+    pinecone.init(api_key=pinecone_key, environment=PINECONE_ENV)
+    #index_description = pinecone.describe_index(pinecone_index)
+    if pinecone_index in pinecone.list_indexes():
+        index = pinecone.Index(pinecone_index)
+        index_stats_response = index.describe_index_stats()
+        st.info(f"The Documents available in index: {list(index_stats_response['namespaces'].keys())}")
     uploaded_files = st.file_uploader("Upload", type=["pdf"], label_visibility="collapsed")#, accept_multiple_files = True
     if uploaded_files is not None:
         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
@@ -115,18 +115,19 @@ def admin():
                     dimension=1536  # 1536 dim of text-embedding-ada-002
                     )
             time.sleep(50)
-            vector_store = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index)
+            vector_store = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index, namespace=uploaded_files.name)
             st.success("Document Uploaded Successfully!")
         elif second_t:
             st.info('Initializing Document Uploading to DB...')
-            vector_store = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index)
+            vector_store = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index, namespace=uploaded_files.name)
             st.success("Document Uploaded Successfully!")
 
 
 def chat():
+
     pinecone_index = "aichat"
     text_field = "text"
-    embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002')
+    embeddings = OpenAIEmbeddings(model='text-embedding-ada-002', openai_api_key='openai_key')
     index = pinecone.Index(pinecone_index)
     db = Pinecone(index, embeddings.embed_query, text_field)
     retriever = db.as_retriever()
@@ -179,55 +180,14 @@ def chat():
 
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])-1, -1, -1):
-            message(st.session_state["generated"][i], key=str(i))
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile",)
     
     if res:
         st.session_state['generated'] = []
         st.session_state['past'] = []
 
-with st.sidebar:
-    st.title("Authenticating Credentials")
-    with st.form("authentication"):
-        openai_api_key = st.text_input(
-            "OpenAI API Key",
-            type="password",
-            #help=OPENAI_HELP,
-            placeholder="This field is mandatory",
-        )
-        PINECONE_API_KEY = st.text_input(
-            "Pinecone API Key",
-            type="password",
-            #help=ACTIVELOOP_HELP,
-            placeholder="This field is mandatory",
-        )
-        PINECONE_ENV = st.text_input(
-            "Pinecone Env",
-            type="password",
-            #help=ACTIVELOOP_HELP,
-            placeholder="This field is mandatory",
-        )
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            authenticate(openai_api_key, PINECONE_API_KEY, PINECONE_ENV)
-
-os.environ["OPENAI_API_KEY"] = openai_api_key
-
-functions = [
-        "Home",
-        "AI Chatbot",
-        "Admin",
-    ]
 
 
 
-selected_function = st.sidebar.selectbox("Select Option", functions)
-if selected_function == "Home":
-    main()
-elif selected_function == "AI Chatbot":
-    chat()
-elif selected_function == "Admin":
-    passw = st.sidebar.text_input("Enter your password: ", type="password")
-    if passw == "ai4chat":
-        admin()
-    
+
