f753e5ed82228c736eab420a516b21b1e4d7450e
S7575 <81707738+S7575@users.noreply.github.com>
2023-07-07 12:22:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ def chat(chat_na):
     if mod:
         pas = st.sidebar.text_input("Write access code", type="password")
         if pas == "ongpt":
-            MODEL_OPTIONS = ["gpt-3.5-turbo-16k-0613", "gpt-4"]
+            MODEL_OPTIONS = ["gpt-3.5-turbo-16k-0613", "gpt-4-32k-0613"]
             model_name = st.sidebar.selectbox(label="Select Model", options=MODEL_OPTIONS)
 
        
